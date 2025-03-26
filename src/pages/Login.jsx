00e31c7ada3fbd8635aa071863aff0dcfae5b22c
nzmshrxz <nazamshiraz4@gmail.com>
2025-03-27 03:49:10 +0530

    updated Login.jsx
    added preventDefault to stop the submit behavior

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,8 @@ const Login = () => {
 
   const navigate = useNavigate()
 
-  function handleClick() {
+  function handleClick(e) {
+    e.preventDefault()
     navigate('/profile')
 
   }
@@ -17,37 +18,37 @@ const Login = () => {
   return (
     <div>
       <Layout>
-        
+
         <div className='container h-[100vh] p-4 w-full'>{/* Main container */}
           <div className='w-[67%]'>
-          <Title Text1={`Signin to your PopX account`}/>
+            <Title Text1={`Signin to your PopX account`} />
           </div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eum ratione itaque  tempore  voluptates obcaecati ex ipsum rerum?</p>
-        <div>
-        <form>
-          
-          <div className="relative w-full pt-4 mb-5">
-            {/* For emaiL  */}
-            <label htmlFor="email" className="absolute left-3 bg-[#F7F8F9] px-1 text-sm text-purple-600">
-              Email
-            </label>
-            <input id="email" className="w-full px-3 py-4 border border-gray-300 rounded-md" type="email" />
+          <div>
+            <form>
+
+              <div className="relative w-full pt-4 mb-5">
+                {/* For emaiL  */}
+                <label htmlFor="email" className="absolute left-3 bg-[#F7F8F9] px-1 text-sm text-purple-600">
+                  Email
+                </label>
+                <input id="email" className="w-full px-3 py-4 border border-gray-300 rounded-md" type="email" />
+              </div>
+              {/* For Password */}
+              <div className="relative w-full">
+                <label htmlFor="password" className="absolute left-3 bg-[#F7F8F9] px-1 text-sm text-purple-600">
+                  Password
+                </label>
+                <input id="password" className="w-full px-3 py-4 border border-gray-300 rounded-md" type="password" />
+              </div>
+              {/* Login Button */}
+              <div className="flex justify-center w-full mt-6">
+                <button onClick={(e) => handleClick(e)} className="bg-[#6C25FF] text-white text-[16px] w-full max-w-[335px] h-[46px] rounded-[6px] cursor-pointer">
+                  Login
+                </button>
+              </div>
+            </form>
           </div>
-          {/* For Password */}
-          <div className="relative w-full">
-            <label htmlFor="password" className="absolute left-3 bg-[#F7F8F9] px-1 text-sm text-purple-600">
-              Password
-            </label>
-            <input id="password" className="w-full px-3 py-4 border border-gray-300 rounded-md" type="password" />
-          </div>
-          {/* Login Button */}
-         <div className="flex justify-center w-full mt-6">
-          <button onClick={()=>handleClick()} className="bg-[#6C25FF] text-white text-[16px] w-full max-w-[335px] h-[46px] rounded-[6px] cursor-pointer">
-            Login
-          </button>
-          </div>
-        </form>
-        </div>
         </div>
       </Layout>
     </div>
